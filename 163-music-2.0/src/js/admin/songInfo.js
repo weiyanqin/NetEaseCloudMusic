028812af0a8044dda9337aa66dd99f39568a23d8
weiyanqin <wanghaiming723@foxmail.com>
2018-10-17 19:28:10 +0800

    163music迭代-完善后端管理页面（2.0）-实现点击歌曲列表，修改歌曲信息功能

--- 163-music-2.0/src/js/admin/songInfo.js
+++ 163-music-2.0/src/js/admin/songInfo.js
@@ -2,9 +2,9 @@
     let view = {
         el: '.info-page',
         template: `
-            <div class="show-area">
+            <form class="show-area">
                 <label>歌曲名称</label>
-                <input type="text" class="song" name="song" value="__name___">
+                <input type="text" class="song" name="name" value="__name___">
                 <label>歌手</label>
                 <input type="text" class="singer" name="singer" value="__singer__">
                 <label>歌词</label>
@@ -12,14 +12,14 @@
                 <label>封面链接</label>
                 <input type="text" class="cover" name="cover" value="http://p1.music.126.net/ZnFe6Uj1Lmr_HrAjDbe02Q==/1394180751920871.jpg?param=130y130">
                 <div class="button-wrapper">
-                    <div class="delete">
+                    <button class="delete">
                         删&nbsp;&nbsp;除
-                    </div>
-                    <div class="confirm">
+                    </button>
+                    <button class="confirm" type="submit">
                         确&nbsp;&nbsp;定
-                    </div>
+                    </button>
                 </div>
-            </div>       
+            </form>       
         `,
         render(data = {}) {
             let placeholders = ['name', 'url']
@@ -36,6 +36,16 @@
             singer: '',
             url: '',
             id: '',
+        },
+        update(data) {
+            var song = AV.Object.createWithoutData('Song', model.data.id)
+            song.set('name', data.name)
+            song.set('singer', data.singer)
+            // song.set('url', data.url)
+            return song.save().then((response)=>{
+                Object.assign(this.data, data)
+                return response
+            })
         }
     }
     let controller = {
@@ -45,12 +55,30 @@
             this.bindEvents()
             this.view.render(this.model.data)
         },
+        update() {
+            let needs = 'name singer'.split(' ')
+            let data = {}
+            needs.map((string) => {
+                data[string] = $(this.view.el).find(`[name = "${string}"]`).val()
+            })
+            this.model.update(data).then(() => {
+                alert('更新成功')
+                console.log(this.model.data)
+                window.eventHub.emit('update', JSON.parse(JSON.stringify(this.model.data)))
+            })
+        },
         bindEvents() {
             window.eventHub.on('select', (data) => {
                 console.log(data)
                 $(this.view.el).removeClass('hide')
                 this.model.data = data
                 this.view.render(this.model.data)
+            })
+            $(this.view.el).on('submit', 'form', (e) => {
+                e.preventDefault()
+                if (this.model.data.id) {
+                    this.update()
+                }
             })
             window.eventHub.on('new', () => {
                 $(this.view.el).addClass('hide')
--- 163-music-2.0/src/js/admin/songList.js
+++ 163-music-2.0/src/js/admin/songList.js
@@ -95,6 +95,18 @@
                 }
                 window.eventHub.emit('select', JSON.parse(JSON.stringify(data)))
             })
+            
+            window.eventHub.on('update', (song)=>{
+                console.log(song)
+                let songs = this.model.data.songs
+                console.log(songs)
+                for(let i=0; i<songs.length; i++){
+                    if(songs[i].id === song.id){
+                        Object.assign(songs[i], song)
+                    }
+                }
+                this.view.render(this.model.data)
+            })
         }
     }
     controller.init(view, model)
